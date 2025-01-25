db513a3f74f5a125edda1e5740ad76b25b46a80b
Hydra <scilentknight512@gmail.com>
2025-01-25 19:10:31 +0545

    stack and heap concept added in datatypes.js file

--- dataTypes.js
+++ dataTypes.js
@@ -13,20 +13,20 @@ let age = 22;
 let isLoggedIn = true;
 let k;
 
-console.log(name);
-console.table([name, age, isLoggedIn]);
+// console.log(name);
+// console.table([name, age, isLoggedIn]);
 
-console.log(typeof age);
-console.log(typeof null);
-console.log(typeof k);
+// console.log(typeof age);
+// console.log(typeof null);
+// console.log(typeof k);
 
 let id = Symbol("123");
 let anotherId = Symbol("123");
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 const BigInt = 36987445588588758885n;
-console.log(BigInt);
-console.log(typeof BigInt);
+// console.log(BigInt);
+// console.log(typeof BigInt);
 
 /*
 Data Types in Javascript
@@ -34,13 +34,13 @@ Data Types in Javascript
 # premitive data type
 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
 
-# Non-premitive data type
+# Non-premitive (Reference) data type
 3 types: Array, Objects, Functions
 */
 
 // Array
 const heros = ["Tony Stark", "Captain America", "The Hulk"];
-console.log(heros);
+// console.log(heros);
 
 // Object
 
@@ -49,12 +49,39 @@ let myObj = {
   age: 50,
   address: "America",
 };
-console.log(myObj);
+// console.log(myObj);
 
 // Finction
 
 const myFunction = function () {
-  console.log("Hello World!");
+  // console.log("Hello World!");
 };
-console.log(typeof myFunction);
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+// Stack and Heap memory in javascript
+// Notes: stack memory used in all premitive data type and heap memory is usedin all non-premitive data type
+// stack (premitive), heap (non=premitive)
+
+let myName = "prakash pathak";
+let newName = myName;
+newName = "manoj kc";
+// changes happen on a copy of the file in stack (premitive data types: Number, BigInt, Symbol, String, Symbol, null, undefined)
+
+console.log(myName);
+console.log(newName);
+
+let userOne = {
+  email: "user@gmail.com",
+  age: 20,
+};
+
+let userTwo = userOne;
+
+userTwo.email = "prakash@gmail.com";
+// changes happen on an original value of the file in heap (non premitive data types: Array, objects, function)
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
+// Notes: stacks gives copy and heap gives reference
