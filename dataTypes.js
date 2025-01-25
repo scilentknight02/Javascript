61b19f5dc64beb407ff8d7812f448b1d9cb0243e
Hydra <scilentknight512@gmail.com>
2025-01-25 18:43:38 +0545

    dataType summary added

--- dataTypes.js
+++ dataTypes.js
@@ -6,7 +6,6 @@
 // null => standalone value
 // symbol => unique
 
-
 // "use strict"; //treat all JS code as newer version
 
 let name = "Prakash";
@@ -20,3 +19,42 @@ console.table([name, age, isLoggedIn]);
 console.log(typeof age);
 console.log(typeof null);
 console.log(typeof k);
+
+let id = Symbol("123");
+let anotherId = Symbol("123");
+console.log(id === anotherId);
+
+const BigInt = 36987445588588758885n;
+console.log(BigInt);
+console.log(typeof BigInt);
+
+/*
+Data Types in Javascript
+
+# premitive data type
+7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+# Non-premitive data type
+3 types: Array, Objects, Functions
+*/
+
+// Array
+const heros = ["Tony Stark", "Captain America", "The Hulk"];
+console.log(heros);
+
+// Object
+
+let myObj = {
+  name: "Tony Stark",
+  age: 50,
+  address: "America",
+};
+console.log(myObj);
+
+// Finction
+
+const myFunction = function () {
+  console.log("Hello World!");
+};
+console.log(typeof myFunction);
+console.log(typeof myFunction);
