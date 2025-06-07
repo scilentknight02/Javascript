24e17af56d04c276b54685a237247219ef14e27d
Scilent Knight <scilentknight512@gmail.com>
2025-06-07 09:23:40 +0545

    fusction scope

--- 03_functions.js
+++ 03_functions.js
@@ -1,2 +1,19 @@
 // function scope
 // global and local scope in javascript
+
+// var global scope can be redeclare and update 
+
+let a = 300
+if (true) {
+  let a = 10;
+  const b = 20; // 
+  var c = 30; // global scope var has a issue which accessible everywhere
+  console.log("INNER: ", a); // block scope
+  
+}
+
+console.log(a) // global scope
+// console.log(b)
+// console.log(c)
+
+
